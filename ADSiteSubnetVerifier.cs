9f45126df36817338b60e6f89889283d6980453e
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-15 14:14:52 -0400

    Initial commit.

---
+++ ADSiteSubnetVerifier.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Diagnostics;
+using System.IO;
+
+namespace ADSiteSubnetVerifier
+{
+    class ADSSVMain
+    {
+        static DirectorySearcher CreateDSSearcher()
+        {
+            // [Comment] Get local domain context
+            string rootDSE;
+
+            System.DirectoryServices.DirectorySearcher objrootDSESearcher = new System.DirectoryServices.DirectorySearcher();
+            rootDSE = objrootDSESearcher.SearchRoot.Path;
+            // [DebugLine]Console.WriteLine(rootDSE);
+
+            // [Comment] Construct DirectorySearcher object using rootDSE string
+            System.DirectoryServices.DirectoryEntry objrootDSEentry = new System.DirectoryServices.DirectoryEntry(rootDSE);
+            System.DirectoryServices.DirectorySearcher objDSSearcher = new System.DirectoryServices.DirectorySearcher(objrootDSEentry);
+            // [DebugLine]Console.WriteLine(objDSSearcher.SearchRoot.Path);
+            return objDSSearcher;
+        }
+
+        static void PrintParameterSyntax()
+        {
+            Console.WriteLine("GroupCopy (c) 2011 SystemsAdminPro.com");
+            Console.WriteLine();
+            Console.WriteLine("Parameter syntax:");
+            Console.WriteLine();
+            Console.WriteLine("Use the following for the first parameter:");
+            Console.WriteLine("-all                for All user objects");
+            Console.WriteLine();
+            Console.WriteLine("Use one of the following as additional parameters:");
+            Console.WriteLine("-count              to include a count with the list");
+            Console.WriteLine("-countonly          to only get a count of the number of users");
+            Console.WriteLine("-ldappath           to get the LDAP path from Active Directory");
+            Console.WriteLine();
+            Console.WriteLine("Examples:");
+            Console.WriteLine("GroupCopy -all");
+            Console.WriteLine("GroupCopy -all -countonly");
+            Console.WriteLine("GroupCopy -all -count -ldappath");
+        }
+
+        static bool LicenseCheck()
+        {
+            string strLicenseString = "";
+            bool bValidLicense = false;
+
+            try
+            {
+                TextReader tr = new StreamReader("sotfwlic.dat");
+
+                try
+                {
+                    strLicenseString = tr.ReadLine();
+
+                    if (strLicenseString.Length > 0 & strLicenseString.Length < 29)
+                    {
+                        // [DebugLine] Console.WriteLine("if: " + strLicenseString);
+                        Console.WriteLine("Invalid license");
+
+                        tr.Close(); // close license file
+
+                        return bValidLicense;
+                    }
+                    else
+                    {
+                        tr.Close(); // close license file
+                        // [DebugLine] Console.WriteLine("else: " + strLicenseString);
+
+                        string strMonthTemp = ""; // to convert the month into the proper number
+                        string strDate;
+
+                        //Month
+                        strMonthTemp = strLicenseString.Substring(7, 1);
+                        if (strMonthTemp == "A")
+                        {
+                            strMonthTemp = "10";
+                        }
+                        if (strMonthTemp == "B")
+                        {
+                            strMonthTemp = "11";
+                        }
+                        if (strMonthTemp == "C")
+                        {
+                            strMonthTemp = "12";
+                        }
+                        strDate = strMonthTemp;
+
+                        //Day
+                        strDate = strDate + "/" + strLicenseString.Substring(16, 1);
+                        strDate = strDate + strLicenseString.Substring(6, 1);
+
+                        // Year
+                        strDate = strDate + "/" + strLicenseString.Substring(24, 1);
+                        strDate = strDate + strLicenseString.Substring(4, 1);
+                        strDate = strDate + strLicenseString.Substring(1, 2);
+
+                        // [DebugLine] Console.WriteLine(strDate);
+                        // [DebugLine] Console.WriteLine(DateTime.Today.ToString());
+                        DateTime dtLicenseDate = DateTime.Parse(strDate);
+                        // [DebugLine]Console.WriteLine(dtLicenseDate.ToString());
+
+                        if (dtLicenseDate >= DateTime.Today)
+                        {
+                            bValidLicense = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("License expired.");
+                        }
+
+                        return bValidLicense;
+                    }
+
+                } //end of try block on tr.ReadLine
+
+                catch
+                {
+                    // [DebugLine] Console.WriteLine("catch on tr.Readline");
+                    Console.WriteLine("Invalid license");
+                    tr.Close();
+                    return bValidLicense;
+
+                } //end of catch block on tr.ReadLine
+
+            } // end of try block on new StreamReader("sotfwlic.dat")
+
+            catch (System.Exception ex)
+            {
+                // [DebugLine] System.Console.WriteLine("{0} exception caught here.", ex.GetType().ToString());
+
+                // [DebugLine] System.Console.WriteLine(ex.Message);
+
+                if (ex.Message.StartsWith("Could not find file"))
+                {
+                    Console.WriteLine("License file not found.");
+                }
+
+                return bValidLicense;
+
+            } // end of catch block on new StreamReader("sotfwlic.dat")
+
+        } // LicenseCheck
+
+        static void LogToEventLog(string strAppName, string strEventMsg, int intEventType)
+        {
+            string sLog;
+
+            sLog = "Application";
+
+            if (!EventLog.SourceExists(strAppName))
+                EventLog.CreateEventSource(strAppName, sLog);
+
+            //EventLog.WriteEntry(strAppName, strEventMsg);
+            EventLog.WriteEntry(strAppName, strEventMsg, EventLogEntryType.Information, intEventType);
+
+        } // LogToEventLog
+
+        static void Main(string[] args)
+        {
+            if (LicenseCheck())
+            {
+                Forest objForest = Forest.GetCurrentForest();
+                ReadOnlySiteCollection objROSC = objForest.Sites;
+
+                foreach (ActiveDirectorySite objADSite in objROSC)
+                {
+                    Console.WriteLine("Site: {0}", objADSite.Name);
+                    Console.WriteLine("Subnet(s) for this site:");
+                    foreach (ActiveDirectorySubnet objADSubnet in objADSite.Subnets)
+                    {
+                        Console.WriteLine(objADSubnet.Name);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
